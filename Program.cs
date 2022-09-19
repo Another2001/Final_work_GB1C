ee861123778286ddde30c6346c12b7a0e27bc8d0
A_not_her <sar.den.art2001@ya.ru>
2022-09-19 13:18:15 +0800

    написан код для решения задачи

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+void Fill(int[,] array, int minValue, int maxValue)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int column = 0; column < columns; column++)
+        {
+            array[row, column] = new Random().Next(minValue, maxValue);
+        }
+    }
+}
+void Print(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int column = 0; column < columns; column++)
+        {
+            Console.Write($"{array[row, column],5}");
+        }
+        Console.WriteLine();
+    }
+}
+int Input(string text)
+{
+    bool f = false;
+    int number = 0;
+    while (!f)
+    {
+        Console.Write(text);
+        string str = Console.ReadLine();
+        f = int.TryParse(str, out number);
+    }
+    return number;
+}
+int[,] Sorting(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int col = 0; col < array.GetLength(1) - 1; col++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[row, k] < array[row, k + 1])
+                {
+                    (array[row, k], array[row, k + 1]) = (array[row, k + 1], array[row, k]);
+                }
+            }
+        }
+    }
+     return array;
+}
+
+int m = Input("rows:");
+int n = Input("columns:");
+if (m >= 0 && n >= 0)
+{
+    int[,] matrix = CreateArray(m, n);
+    Fill(matrix, 10, 20);
+    Print(matrix);
+    Console.WriteLine();
+    Sorting(matrix);
+    Print(matrix);
+}
--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = Input("Введите размер массива : ");
+string[] fullmas = new string[N];
+string masstr = string.Empty;
+Console.WriteLine("Введите строки: ");
+
+int Input(string text)
+{
+    bool f = false;
+    int number = 0;
+    while (!f)
+    {
+        Console.Write(text);
+        string str = Console.ReadLine();
+        f = int.TryParse(str, out number);
+    }
+    return number;
+}
+
+string WritString()
+{
+    string str = Console.ReadLine();
+    return str;
+}
+
+for (int i = 0; i < N; i++)
+{
+    fullmas[i] = WritString();
+    masstr += fullmas[i] + " ";
+}
+
+string Formating()
+{
+    string str = string.Empty;
+    for (int i = 0; i < N; i++)
+    {
+        if (fullmas[i].Length <= 3)
+        {
+            str += fullmas[i] + " ";
+        }
+    }
+    return str;
+}
+
+Console.WriteLine($"полученный массив строк [{masstr}]");
+Console.WriteLine("строки длиной <=3 символов:");
+Console.Write($"[{Formating()}]");
+
+
+
